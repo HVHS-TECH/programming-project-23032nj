da7f597bf58753154de1318be54cb599846cace4
23032nj <23032nj@hvhs.school.nz>
2026-03-17 20:53:51 +1300

    saving changes 17/03

--- block_game.js
+++ block_game.js
@@ -8,7 +8,7 @@
 const GAMEWIDTH = 650;
 const GAMEHEIGHT = 700;
 const BALLDIAMETER = 30;
-const PLATFORMXPOSITION = GAMEWIDTH/2;
+const PLATFORMXPOSITION = GAMEWIDTH / 2;
 const PLATFORMYPOSITION = GAMEHEIGHT - 70;
 const PLATFORMWIDTH = 130;
 const PLATFORMHEIGHT = 5;
@@ -26,65 +26,65 @@ let colorArray = ['#febdb1', '#d6fda9', '#a8deff'];
 /*******************************************************/
 
 function preload() {
-imgBall = loadImage('../assets/images/ballimg.png');
-}	
+  imgBall = loadImage('../assets/images/ballimg.png');
+}
 
 /*******************************************************/
 // setup()
 /*******************************************************/
 
 function setup() {
-console.log("running game");
-cnv = new Canvas(GAMEWIDTH, GAMEHEIGHT);
-world.gravity.y = 0;
+  console.log("running game");
+  cnv = new Canvas(GAMEWIDTH, GAMEHEIGHT);
+  world.gravity.y = 0;
 
-//defining variables within setup
-randNum = random(10, GAMEWIDTH - 10);
+  //defining variables within setup
+  randNum = random(10, GAMEWIDTH - 10);
 
-//running walls function
-walls() 
+  //running walls function
+  walls()
 
-// ball code
-ball = new Sprite(GAMEWIDTH/2, PLATFORMYPOSITION - BALLDIAMETER, BALLDIAMETER,'d');
-ball.color = '#c587dd';
-ball.image = (imgBall);
-imgBall.resize(BALLDIAMETER, BALLDIAMETER);
+  // ball code
+  ball = new Sprite(GAMEWIDTH/2, PLATFORMYPOSITION - BALLDIAMETER, BALLDIAMETER, 'd');
+  ball.color = '#c587dd';
+  ball.image = (imgBall);
+  imgBall.resize(BALLDIAMETER, BALLDIAMETER);
 
-//platform code
-platform = new Sprite(PLATFORMXPOSITION, PLATFORMYPOSITION, PLATFORMWIDTH, PLATFORMHEIGHT, 'k');
-platform.color = '#698fe7';
+  //platform code
+  platform = new Sprite(PLATFORMXPOSITION, PLATFORMYPOSITION, PLATFORMWIDTH, PLATFORMHEIGHT, 'k');
+  platform.color = '#698fe7';
 
-//blocks code
-blockCreate ()
+  //blocks code
+  blockCreate()
 
-// deleting blocks 
-blockGroup.collides(ball, func2Call);
+  // deleting blocks 
+  blockGroup.collides(ball, func2Call);
 
-function func2Call(block, ball) {
-block.remove();
-score = score + 1;
-console.log(score);
-} 
+  function func2Call(block, ball) {
+    block.remove();
+    score = score + 1;
+    console.log(score);
+  }
 
-//setup() finished
+  //setup() finished
 }
 
 /*******************************************************/
 // walls()
 /*******************************************************/
 
-function walls () {
-wallLeft  = new Sprite(WALLDEPTH/2, height/2, WALLDEPTH, height, 'k');
-wallLeft.color = '#eb7184';
+function walls() {
+  wallLeft = new Sprite(WALLDEPTH / 2, height / 2, WALLDEPTH, height, 'k');
+  wallLeft.color = '#eb7184';
 
-wallRight  = new Sprite(GAMEWIDTH - WALLDEPTH/2, height/2, WALLDEPTH, height, 'k');
-wallRight.color = '#eb7184';
+  wallRight = new Sprite(GAMEWIDTH - WALLDEPTH / 2, height / 2, WALLDEPTH, height, 'k');
+  wallRight.color = '#eb7184';
 
-wallTop = new Sprite(width/2, WALLDEPTH/2, width, WALLDEPTH, 'k');
-wallTop.color = '#eb7184';
+  wallTop = new Sprite(width / 2, WALLDEPTH / 2, width, WALLDEPTH, 'k');
+  wallTop.color = '#eb7184';
 
-wallBottom = new Sprite(width/2, GAMEHEIGHT - WALLDEPTH/2, width, WALLDEPTH, 'k');
-wallBottom.color = '#eb7184';
+  wallBottom = new Sprite(width / 2, GAMEHEIGHT - WALLDEPTH / 2, width, WALLDEPTH, 'k');
+  wallBottom.color = '#eb7184';
 }
 
 
@@ -92,82 +92,82 @@ wallBottom.color = '#eb7184';
 // blockCreate()
 /*******************************************************/
 
-function blockCreate () {
- blockGroup = new Group();
- for (var row = 0; row < 4; row++) {
-   for (var i = 0; i < 7; i++) {
-     var block = new Sprite(i*80 + 83, row*45 + 75, BLOCKWIDTH, BLOCKHEIGHT, 'k');
-     block.color = blockRowColor;
-     blockGroup.add(block);
+function blockCreate() {
+  blockGroup = new Group();
+  for (var row = 0; row < 4; row++) {
+    for (var i = 0; i < 7; i++) {
+      var block = new Sprite(i * 80 + 83, row * 45 + 75, BLOCKWIDTH, BLOCKHEIGHT, 'k');
+      block.color = blockRowColor;
+      blockGroup.add(block);
     }
-   blockRowColor = colorArray[row];
- }
+    blockRowColor = colorArray[row];
+  }
 }
 
 /*******************************************************/
 // draw()
 /*******************************************************/
-function draw() {		
-background ('#bfd7fa');
+function draw() {
+  background('#bfd7fa');
 
-//score display
-text('Score: ' + score, GAMEWIDTH - 150 , 40 );
-textSize(25);
-fill('#eb7184');
+  //score display
+  text('Score: ' + score, GAMEWIDTH - 150, 40);
+  textSize(25);
+  fill('#eb7184');
 
-//moving the platform
-if (kb.pressing('left')) {
-platform.vel.x = '-8';
-}
-else if (kb.pressing ('right')) {
-platform.vel.x = '8';
-}
+  //moving the platform
+  if (kb.pressing('left')) {
+    platform.vel.x = '-8';
+  }
+  else if (kb.pressing('right')) {
+    platform.vel.x = '8';
+  }
 
-if (kb.released('left')) {
-platform.vel.x = '0';
-}
+  if (kb.released('left')) {
+    platform.vel.x = '0';
+  }
 
-if (kb.released('right')) {
-platform.vel.x = '0';
-}
+  if (kb.released('right')) {
+    platform.vel.x = '0';
+  }
 
-if (platform.x >= GAMEWIDTH - PLATFORMWIDTH/2 - 5) {
-platform.x = GAMEWIDTH - PLATFORMWIDTH/2 - WALLDEPTH;
-}
+  if (platform.x >= GAMEWIDTH - PLATFORMWIDTH / 2 - 5) {
+    platform.x = GAMEWIDTH - PLATFORMWIDTH / 2 - WALLDEPTH;
+  }
 
-if (platform.x < WALLDEPTH + PLATFORMWIDTH/2) {
-platform.x = WALLDEPTH + PLATFORMWIDTH/2;
-}
+  if (platform.x < WALLDEPTH + PLATFORMWIDTH / 2) {
+    platform.x = WALLDEPTH + PLATFORMWIDTH / 2;
+  }
 
-//Once space is pressed
-if (kb.presses('space')) {
-ball.bounciness = 1;
-ball.vel.y = -5;
-ball.friction = 0;
-ball.drag = 0;
-}
+  //Once space is pressed
+  if (kb.presses('space')) {
+    ball.bounciness = 1;
+    ball.vel.y = -5;
+    ball.friction = 0;
+    ball.drag = 0;
+  }
 
-//Game ends when the ball hits the bottom
-ball.collides (wallBottom, functionGameEnd) 
+  //Game ends when the ball hits the bottom
+  ball.collides(wallBottom, functionGameEnd)
 
-function functionGameEnd (wallBottom, Ball) {
- ball.remove();
- console.log("Game over. You got " + score + " points.");
- gameEnd.style.display = "block";
-}
+  function functionGameEnd(wallBottom, Ball) {
+    ball.remove();
+    console.log("Game over. You got " + score + " points.");
+    gameEnd.style.display = "block";
+  }
 
-//When all the blocks have been deleted
-if (blockGroup.length = 0) {
- blockCreate () 
-}
+  //When all the blocks have been deleted
+  if (blockGroup.length == 0) {
+    blockCreate()
+  }
 
-//Endscreen code
-p_heading.textContent = "You lost!";
-p_score.textContent = "You got " + score + " points. Congratulations";
-p_replay.textContent = "To try again click 'retry' ";
+  //Endscreen code
+  p_heading.textContent = "You lost!";
+  p_score.textContent = "You got " + score + " points. Congratulations";
+  p_replay.textContent = "To try again click 'retry' ";
 
 
-//End of draw loop
+  //End of draw loop
 }
 
 /***********************************************/
